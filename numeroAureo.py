1c521d922d4e6b0b7d614c2968e5c7643388b500
Héctor Selley <hector.selley@gmail.com>
2025-05-05 19:50:02 -0600

    Código completado
    Programa que aproxima el número aureo

---
+++ numeroAureo.py
@@ -0,0 +1,19 @@
+'''
+Sucesión de Fibonacci versión lista
+La función recibe como argumento la cantidad de cifras de la serie
+
+El cálculo del número aureo está dado por:
+\varphi = lim_{n\rightarrow \infty} = \dfrac{Fib(n+1)}{n}
+'''
+
+def Fib(n=10):
+    lista = [0, 1]
+    for _ in range(n):
+        lista.append(lista[-1] + lista[-2])
+    return lista
+
+# print(Fib(5000))
+
+n=10000
+sucesion = Fib(n)
+print(sucesion[-1]/sucesion[-2])
